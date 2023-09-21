f66cc76c46b3e5bb8e1fc70f06d88105a8edf02e
manish-neemnarayan <manish9301kumar@gmail.com>
2023-09-21 22:44:56 +0530

    FEAT: seat model and migration added

---
+++ src/migrations/20230921135820-create-seat.js
@@ -0,0 +1,42 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Seats', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      airplaneId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      row: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      col: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      type: {
+        type: DataTypes.ENUM,
+        values: [BUSINESS, ECONOMY, PREMIUM_ECONOMY, FIRST_CLASS],
+        defaultValue: ECONOMY
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Seats');
+  }
+};
